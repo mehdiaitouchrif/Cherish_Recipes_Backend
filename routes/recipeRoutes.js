79aca73d6da940d22a70b9addcc395f604859efe
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-01-30 00:06:20 +0100

    Get user recipes route

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,4 +1,20 @@
 import express from 'express'
-const router = express.Router()
+import {
+	createRecipe,
+	deleteRecipe,
+	getRecipe,
+	getRecipes,
+	updateRecipe,
+} from '../controllers/recipeController.js'
+import requireAuth from '../middleweare/requireAuth.js'
+
+const router = express.Router({ mergeParams: true })
+
+router.route('/').post(requireAuth, createRecipe).get(getRecipes)
+router
+	.route('/:id')
+	.get(getRecipe)
+	.put(requireAuth, updateRecipe)
+	.delete(requireAuth, deleteRecipe)
 
 export default router
