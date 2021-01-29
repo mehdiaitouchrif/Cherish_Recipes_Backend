f8c396dfbb5f8bb0d5eac0e2a71b5277826c809a
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-01-29 19:56:38 +0100

    Create all authentication controller methods

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import {
+	forgotPassword,
 	getCurrentUser,
+	updateDetails,
+	updatePassword,
+	resetPassword,
 	signIn,
 	signUp,
+	logout,
+	deleteAccount,
+	confirmEmail,
+	sendConfirmationEmail,
 } from '../controllers/authController.js'
 import requireAuth from '../middleweare/requireAuth.js'
 
@@ -11,4 +19,13 @@ const router = express.Router()
 router.post('/signup', signUp)
 router.post('/signin', signIn)
 router.get('/me', requireAuth, getCurrentUser)
+router.get('/logout', requireAuth, logout)
+router.put('/updatedetails', requireAuth, updateDetails)
+router.put('/updatepassword', requireAuth, updatePassword)
+router.post('/forgotpassword', forgotPassword)
+router.put('/resetpassword/:resettoken', resetPassword)
+router.post('/sendconfirmationemail/', requireAuth, sendConfirmationEmail)
+router.put('/confirmemail/:confirmationtoken', confirmEmail)
+router.delete('/deleteAccount', requireAuth, deleteAccount)
+
 export default router
