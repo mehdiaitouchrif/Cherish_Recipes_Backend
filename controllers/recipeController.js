850f8967522017d551d72a12d2b9dcf322854662
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-01-30 12:24:05 +0100

    Revert "Add recipe to favorites functionality"
    This reverts commit 61bbc5615fab27e19c51dd995e5d5128ffe8d8a3.

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,17 +1,13 @@
 import Recipe from '../models/Recipe.js'
 import asyncHandler from '../middleweare/asyncHandler.js'
 import ErrorResponse from '../utils/errorResponse.js'
-import User from '../models/User.js'
 
 // List all recipes
 // GET /api/v1/recipes | Public
 // GET /api/v1/auth/:userId/recipes
 export const getRecipes = asyncHandler(async (req, res) => {
 	if (req.params.userId) {
-		const recipes = await Recipe.find({ user: req.params.userId }).populate(
-			'user',
-			'firstName lastName'
-		)
+		const recipes = await Recipe.find({ user: req.params.userId })
 		res.status(200).json({
 			success: true,
 			count: recipes.length,
@@ -38,24 +34,6 @@ export const getRecipe = asyncHandler(async (req, res, next) => {
 
 	if (recipe) {
 		res.json({
-			success: true,
-			data: recipe,
-		})
-	} else {
-		next(new ErrorResponse('No recipe found', 404))
-	}
-})
-
-// Add favorite recipes
-// PUT /api/v1/recipes/favorites/:recipeId
-export const addToFavorites = asyncHandler(async (req, res, next) => {
-	let user = await User.findById(req.user._id)
-
-	const recipe = await Recipe.findById(req.params.recipeId)
-	if (recipe) {
-		user.favoriteRecipes.push(recipe)
-		await user.save()
-		res.status(200).json({
 			success: true,
 			data: recipe,
 		})
